427233395e636c6957530d23eff585b6e3a061bb
Dhruva Sharma <dhruvasharma.work@gmail.com>
2026-06-13 22:12:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+#selection Sorting program Basic
 
+from typing import List
+
+class Solution:
+    def selectionsort(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        for i in range(n):
+            min_index = i
+
+            for j in range(i + 1, n):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+
+            nums[i], nums[min_index] = nums[min_index], nums[i]
+
+        return nums
+
+
+obj = Solution()
+print(obj.selectionsort([64, 25, 12, 22, 11]))
